8dbb3ea42e5da88e4f6a3037f5496f9ff11b7fa5
Heisenberg976 <77876435+Heisenberg976@users.noreply.github.com>
2021-09-10 16:14:48 +0300

    Update keep-clone.sql

--- keep-clone.sql
+++ keep-clone.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+ -- phpMyAdmin SQL Dump
 -- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2021 at 03:10 PM
+-- Generation Time: Sep 10, 2021 at 03:14 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -33,16 +33,6 @@ CREATE TABLE `tags` (
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `tags`
---
-
-INSERT INTO `tags` (`id`, `userid`, `name`) VALUES
-(1, 1, 'code'),
-(9, 1, 'test1233'),
-(15, 1, '123'),
-(17, 1, 'personal123');
-
 -- --------------------------------------------------------
 
 --
@@ -56,15 +46,6 @@ CREATE TABLE `todos` (
   `body` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `todos`
---
-
-INSERT INTO `todos` (`id`, `userid`, `name`, `body`) VALUES
-(1, 1, 'schedule', 'This weekendd'),
-(9, 1, 'tomorrow', 'abcde'),
-(26, 1, ' nika ', ' 123 ');
-
 -- --------------------------------------------------------
 
 --
@@ -77,17 +58,6 @@ CREATE TABLE `todotags` (
   `tagid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `todotags`
---
-
-INSERT INTO `todotags` (`id`, `todoid`, `tagid`) VALUES
-(34, 25, 1),
-(59, 9, 1),
-(62, 26, 1),
-(63, 26, 15),
-(64, 26, 17);
-
 -- --------------------------------------------------------
 
 --
@@ -99,15 +69,6 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `email`, `password`) VALUES
-(1, 'nika@gmail.com', '0192023a7bbd73250516f069df18b500'),
-(8, 'user', '6ad14ba9986e3615423dfca256d04e3f'),
-(9, 'nika@gmail.com', '702d2dc2c5e7d25d7a05d6dc866cd919');
 
 --
 -- Indexes for dumped tables
@@ -145,25 +106,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `todos`
 --
 ALTER TABLE `todos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `todotags`
 --
 ALTER TABLE `todotags`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
